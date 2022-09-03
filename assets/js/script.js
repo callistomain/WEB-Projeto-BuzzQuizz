082b27ea359cb413e2e5f322d6cb1458a7015244
Sabta Mendes <sabtaamendes@gmail.com>
2022-09-03 19:26:16 -0400

    inicio do bônus de exclusão de quizz

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,61 +9,61 @@ renderMainPage();
 
 // Functions ================================================================================
 function renderMainPage() {
-    // Render all-quizzes
+	// Render all-quizzes
 	const allQuizzesList = document.querySelector(".all-quizzes ul");
-    let fragment;
-    
-    startLoading(list);
+	let fragment;
+
+	startLoading(list);
 	axios.get(url)
-    .then(promise => {
-        const allQuizzes = promise.data;
-        const userList = JSON.parse(localStorage.getItem("userList"));
-        
-        // Filtering user quizzes
-        if (userList) {
-            const myQuizzesList = document.querySelector(".my-quizzes ul");
-            document.querySelector(".empty-quizz").classList.add("hidden");
-            document.querySelector(".my-title").classList.remove("hidden");
-            const myQuizzes = [];
+		.then(promise => {
+			const allQuizzes = promise.data;
+			const userList = JSON.parse(localStorage.getItem("userList"));
 
-            for (let i = 0; i < allQuizzes.length; i++) {
-                let isFromUser = false;
+			// Filtering user quizzes
+			if (userList) {
+				const myQuizzesList = document.querySelector(".my-quizzes ul");
+				document.querySelector(".empty-quizz").classList.add("hidden");
+				document.querySelector(".my-title").classList.remove("hidden");
+				const myQuizzes = [];
 
-                for (let j = 0; j < userList.length; j++) {
-                    if (allQuizzes[i].id === userList[j]) {
-                        userList.splice(j, 1);
-                        isFromUser = true;
-                        break;
-                    }
-                }
+				for (let i = 0; i < allQuizzes.length; i++) {
+					let isFromUser = false;
 
-                if (isFromUser) {
-                    myQuizzes.push(allQuizzes[i]);
-                    allQuizzes.splice(i, 1);
-                    i--;
-                }
-            }
+					for (let j = 0; j < userList.length; j++) {
+						if (allQuizzes[i].id === userList[j]) {
+							userList.splice(j, 1);
+							isFromUser = true;
+							break;
+						}
+					}
 
-            // My Quizzes
-            fragment = document.createDocumentFragment();
-            myQuizzes.forEach(e => fragment.appendChild(createQuizzBox(e)));
-            myQuizzesList.replaceChildren(fragment);
-        }
+					if (isFromUser) {
+						myQuizzes.push(allQuizzes[i]);
+						allQuizzes.splice(i, 1);
+						i--;
+					}
+				}
 
-        // All Quizzes
-        fragment = document.createDocumentFragment();
-        allQuizzes.forEach(e => fragment.appendChild(createQuizzBox(e)));
-        allQuizzesList.replaceChildren(fragment);
+				// My Quizzes
+				fragment = document.createDocumentFragment();
+				myQuizzes.forEach(e => fragment.appendChild(createQuizzBox(e)));
+				myQuizzesList.replaceChildren(fragment);
+			}
 
-        endLoading();
-    });
+			// All Quizzes
+			fragment = document.createDocumentFragment();
+			allQuizzes.forEach(e => fragment.appendChild(createQuizzBox(e)));
+			allQuizzesList.replaceChildren(fragment);
+
+			endLoading();
+		});
 }
 
 // Page 1 > Page 3
 function toQuizzCreate() {
 	list.classList.add("hidden");
 	create.classList.remove("hidden");
-	create.innerHTML=`
+	create.innerHTML = `
 	<p class="title-creation" >Comece pelo começo</p>
 	<div class="box-creation">
 	  <input class='creation-space-1' type="text" id="text" placeholder="Título do seu quizz" required>
@@ -72,28 +72,28 @@ function toQuizzCreate() {
 	  <input class='creation-space-1' type="number" id="number2" placeholder="Quantidade de níveis do quizz" min="0" required>
 	</div>
 	<button onclick="validarInfosQuizz()">Prosseguir pra criar perguntas</button>`
-    window.scrollTo(0, 0);
+	window.scrollTo(0, 0);
 }
 
 // Page 1 > Page 2
 function toQuizzPage(e) {
-    list.classList.add("hidden");
+	list.classList.add("hidden");
 	page.classList.remove("hidden");
-    window.scrollTo(0, 0);
-    
-    startLoading(page);
+	window.scrollTo(0, 0);
+
+	startLoading(page);
 	axios.get(url + "/" + e.currentTarget.id)
-	.then(promise => {
-        renderizarQuizz(promise.data);
-        endLoading();
-    });
+		.then(promise => {
+			renderizarQuizz(promise.data);
+			endLoading();
+		});
 }
 
 function pageToHome() {
-    page.classList.add("hidden");
-    list.classList.remove("hidden");
-    window.scrollTo(0, 0);
-    renderMainPage();
+	page.classList.add("hidden");
+	list.classList.remove("hidden");
+	window.scrollTo(0, 0);
+	renderMainPage();
 }
 
 // DOM ======================================================================================
@@ -121,14 +121,14 @@ function createQuizzBox(obj) {
 
 // HELPERS ==================================================================================
 function startLoading(element) {
-    toLoad = element;
-    loading.classList.remove("hidden");
-    toLoad.classList.add("hidden");
+	toLoad = element;
+	loading.classList.remove("hidden");
+	toLoad.classList.add("hidden");
 }
 
 function endLoading() {
-    loading.classList.add("hidden");
-    toLoad.classList.remove("hidden");
+	loading.classList.add("hidden");
+	toLoad.classList.remove("hidden");
 }
 
 // Comportamento das respostas ==============================================================
@@ -136,8 +136,8 @@ const delay = 100; // 2000
 let acertos, selecionados, quizzAtual;
 
 function renderizarQuizz(quizz) {
-    acertos = 0;
-    selecionados = 0;
+	acertos = 0;
+	selecionados = 0;
 	quizzAtual = quizz;
 
 	let perguntas = '';
@@ -246,15 +246,15 @@ function resolveResult() {
 }
 
 function resetQuizz() {
-    renderizarQuizz(quizzAtual);
-    window.scrollTo(0, 0);
+	renderizarQuizz(quizzAtual);
+	window.scrollTo(0, 0);
 }
 
 // Create Quizz ==================================================================
 let arrayCreateQuizz = [];
-let newQuizzData=[];
+let newQuizzData = [];
 function goToCreateQuestion() {
-	newQuizzData=[];
+	newQuizzData = [];
 	const listInputs = create.querySelectorAll('input');
 	for (let i = 0; i < listInputs.length; i++) {
 		newQuizzData.push(listInputs[i].value);
@@ -292,9 +292,9 @@ function goToCreateQuestion() {
 	create.innerHTML = create.innerHTML + `<button onclick="validityQuestions()" >Prosseguir pra criar níveis</button>`
 	const ion = document.querySelector('.quizz-create ion-icon');
 	toggleQuestion(ion.parentNode);
-	window.scrollTo(0,0);
+	window.scrollTo(0, 0);
 }
-function validityQuestions(){
+function validityQuestions() {
 	let valityValue = 1;
 	let isAllEmpty = 1;
 	let html = "<h1>My new paragraph.</h1>";
@@ -304,95 +304,96 @@ function validityQuestions(){
 	let allH1 = create.querySelectorAll('h1');
 	let allH2 = create.querySelectorAll('h2');
 	let allH3 = create.querySelectorAll('h3');
-	for (let i=0; i<allInputs.length;i++){
+	for (let i = 0; i < allInputs.length; i++) {
 		allInputs[i].classList.remove('become-red');
 		allInputs[i].classList.remove('become-red-2');
 		allInputs[i].classList.remove('become-red-3');
 	}
-	for (i=0; i<internalBox.length;i++){
+	for (i = 0; i < internalBox.length; i++) {
 		internalBox[i].parentNode.classList.remove('error');
 	}
-	for (i=0; i<allH1.length;i++){
+	for (i = 0; i < allH1.length; i++) {
 		allH1[i].remove();
 	}
-	for (i=0; i<allH3.length;i++){
+	for (i = 0; i < allH3.length; i++) {
 		allH3[i].remove();
 	}
-	for (i=0; i<allH2.length;i++){
+	for (i = 0; i < allH2.length; i++) {
 		allH2[i].remove();
 	}
-	for (i=0; i<internalBox.length;i++){
+	for (i = 0; i < internalBox.length; i++) {
 		isAllEmpty = 1;
 		inputsBoxes = internalBox[i].querySelectorAll('input');
-		if (inputsBoxes[0].value.length<=20){
+		if (inputsBoxes[0].value.length <= 20) {
 			internalBox[i].parentNode.classList.add('error');
 			inputsBoxes[0].classList.add('become-red');
 			valityValue = 0;
 			openQuestion(internalBox[i].parentNode.querySelector('.external'));
 			inputsBoxes[0].insertAdjacentHTML("afterend", "<h1>O texto deve ter mais que 20 caracteres.</h1>");
 		}
-		for (ii=4;ii<inputsBoxes.length;ii++){
-			if (inputsBoxes[ii].value!='')
-				isAllEmpty=0;
+		for (ii = 4; ii < inputsBoxes.length; ii++) {
+			if (inputsBoxes[ii].value != '')
+				isAllEmpty = 0;
 		}
-		if (inputsBoxes[2].value===''&&inputsBoxes[3].value===''){
+		if (inputsBoxes[2].value === '' && inputsBoxes[3].value === '') {
 			internalBox[i].parentNode.classList.add('error');
 			inputsBoxes[2].classList.add('become-red');
 			inputsBoxes[3].classList.add('become-red');
 			inputsBoxes[2].insertAdjacentHTML("beforebegin", "<h3>Coloque uma resposta correta.</h3>");
-		}else{
-			if(!inputsBoxes[2].checkValidity()){
+		} else {
+			if (!inputsBoxes[2].checkValidity()) {
 				internalBox[i].parentNode.classList.add('error');
 				inputsBoxes[2].classList.add('become-red');
 				openQuestion(internalBox[i].parentNode.querySelector('.external'));
 				inputsBoxes[2].insertAdjacentHTML("afterend", "<h1>Este campo não pode ficar vazio.</h1>");
 			}
-			if(!inputsBoxes[3].checkValidity()){
+			if (!inputsBoxes[3].checkValidity()) {
 				internalBox[i].parentNode.classList.add('error');
 				inputsBoxes[3].classList.add('become-red');
 				openQuestion(internalBox[i].parentNode.querySelector('.external'));
 				inputsBoxes[3].insertAdjacentHTML("afterend", "<h1>Coloque uma URL valida.</h1>");
-			}}
-		if (isAllEmpty){
+			}
+		}
+		if (isAllEmpty) {
 			inputsBoxes[4].insertAdjacentHTML("beforebegin", "<h2>Coloque pelo menos uma resposta incorreta.</h2>");
-			for (ii=4;ii<inputsBoxes.length;ii++){
+			for (ii = 4; ii < inputsBoxes.length; ii++) {
 				internalBox[i].parentNode.classList.add('error');
 				inputsBoxes[ii].classList.add('become-red-2');
 				openQuestion(internalBox[i].parentNode.querySelector('.external'));
 			}
 		}
-		else{
-			if (inputsBoxes[4].value!==''||inputsBoxes[5].value!==''){
-				if (inputsBoxes[4].value===''){
+		else {
+			if (inputsBoxes[4].value !== '' || inputsBoxes[5].value !== '') {
+				if (inputsBoxes[4].value === '') {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[4].classList.add('become-red-3');
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
 					inputsBoxes[4].insertAdjacentHTML("afterend", "<h1>Este campo não pode ficar vazio.</h1>");
-				}if(!inputsBoxes[5].checkValidity()){
+				} if (!inputsBoxes[5].checkValidity()) {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[5].classList.add('become-red');
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
 					inputsBoxes[5].insertAdjacentHTML("afterend", "<h1>Coloque uma URL valida.</h1>");
 				}
-			}if (inputsBoxes[6].value!==''||inputsBoxes[7].value!==''){
-				if (inputsBoxes[6].value===''){
+			} if (inputsBoxes[6].value !== '' || inputsBoxes[7].value !== '') {
+				if (inputsBoxes[6].value === '') {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[6].classList.add('become-red-3');
 					inputsBoxes[6].insertAdjacentHTML("afterend", "<h1>Este campo não pode ficar vazio.</h1>");
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
-				}if(!inputsBoxes[7].checkValidity()){
+				} if (!inputsBoxes[7].checkValidity()) {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[7].classList.add('become-red');
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
 					inputsBoxes[7].insertAdjacentHTML("afterend", "<h1>Coloque uma URL valida.</h1>");
 				}
-			}if (inputsBoxes[8].value!==''||inputsBoxes[9].value!==''){
-				if (inputsBoxes[8].value===''){
+			} if (inputsBoxes[8].value !== '' || inputsBoxes[9].value !== '') {
+				if (inputsBoxes[8].value === '') {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[8].classList.add('become-red-3');
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
 					inputsBoxes[8].insertAdjacentHTML("afterend", "<h1>Este campo não pode ficar vazio.</h1>");
-				}if(!inputsBoxes[9].checkValidity()){
+				} if (!inputsBoxes[9].checkValidity()) {
 					internalBox[i].parentNode.classList.add('error');
 					inputsBoxes[9].classList.add('become-red');
 					openQuestion(internalBox[i].parentNode.querySelector('.external'));
@@ -401,17 +402,18 @@ function validityQuestions(){
 			}
 		}
 	}
-	for (ii=0; ii<internalBox.length;ii++){
-		if (!internalBox[ii].parentNode.classList.contains('error')){
-			closeQuestion(internalBox[ii].parentNode.querySelector('.external'));}
+	for (ii = 0; ii < internalBox.length; ii++) {
+		if (!internalBox[ii].parentNode.classList.contains('error')) {
+			closeQuestion(internalBox[ii].parentNode.querySelector('.external'));
+		}
 	}
 	if (valityValue === 1)
 		goToCreateLevel();
 	else
-		window.scrollTo(0,0);
+		window.scrollTo(0, 0);
 }
 
-function goToCreateLevel(){
+function goToCreateLevel() {
 	create.innerHTML = `<p class="title-creation">Agora, decida os níveis!</p>`;
 	for (let i = 0; i < newQuizzData[3]; i++) {
 		create.innerHTML += `
@@ -435,9 +437,9 @@ function goToCreateLevel(){
 	create.innerHTML = create.innerHTML + `<button onclick="validityLevels()" >Finalizar Quizz</button>`
 	const ion = document.querySelector('.quizz-create ion-icon');
 	toggleQuestion(ion.parentNode);
-	window.scrollTo(0,0);
+	window.scrollTo(0, 0);
 }
-function validityLevels(){
+function validityLevels() {
 	let valityValue = 1;
 	let isAllEmpty = 1;
 	let thereA0Percent = 0;
@@ -449,50 +451,50 @@ function validityLevels(){
 	let allH2 = create.querySelectorAll('h2');
 	let allH3 = create.querySelectorAll('h3');
 	inputsBoxes = create.querySelectorAll('input');
-	for (let i=0; i<allInputs.length;i++){
+	for (let i = 0; i < allInputs.length; i++) {
 		allInputs[i].classList.remove('become-red');
 		allInputs[i].classList.remove('become-red-2');
 		allInputs[i].classList.remove('become-red-3');
 	}
-	for (i=0; i<allH1.length;i++){
+	for (i = 0; i < allH1.length; i++) {
 		allH1[i].remove();
 	}
-	for (i=0; i<allText.length;i++){
+	for (i = 0; i < allText.length; i++) {
 		allText[i].classList.remove('become-red');
 	}
-	for (i=0; i<allH2.length;i++){
+	for (i = 0; i < allH2.length; i++) {
 		allH2[i].remove();
 	}
-	for (i=0; i<allH3.length;i++){
+	for (i = 0; i < allH3.length; i++) {
 		allH3[i].remove();
 	}
-	for (i=0; i<internalBox.length;i++){
+	for (i = 0; i < internalBox.length; i++) {
 		internalBox[i].parentNode.classList.remove('error');
 	}
-	for (i=0; i<internalBox.length;i++){
-		if(internalBox[i].querySelector("#number").value==0||internalBox[i].querySelector("#number").value===''){
+	for (i = 0; i < internalBox.length; i++) {
+		if (internalBox[i].querySelector("#number").value == 0 || internalBox[i].querySelector("#number").value === '') {
 			thereA0Percent = 1;
 		}
 	}
-	if(thereA0Percent === 0){
-		for (i=0; i<internalBox.length;i++){
-			internalBox[i].querySelector('input').insertAdjacentHTML('beforebegin',"<h3>Deve ter pelo menos um nível minimo 0.</h3>");
+	if (thereA0Percent === 0) {
+		for (i = 0; i < internalBox.length; i++) {
+			internalBox[i].querySelector('input').insertAdjacentHTML('beforebegin', "<h3>Deve ter pelo menos um nível minimo 0.</h3>");
 			internalBox[i].querySelector("#number").classList.add('become-red');
 			internalBox[i].parentNode.classList.add('error');
 			valityValue = 0;
 		}
 	}
 
-	for (i=0; i<internalBox.length;i++){
+	for (i = 0; i < internalBox.length; i++) {
 		isAllEmpty = 1;
 		inputsBoxes = internalBox[i].querySelectorAll('input');
-		if (inputsBoxes[0].value.length<=10) {
+		if (inputsBoxes[0].value.length <= 10) {
 			internalBox[i].parentNode.classList.add('error');
 			openQuestion(internalBox[i].parentNode.querySelector('.external'));
 			inputsBoxes[0].classList.add('become-red');
 			valityValue = 0;
 			inputsBoxes[0].insertAdjacentHTML("afterend", "<h1>O título do nível deve ter no mínimo 10 caracteres.</h1>");
-		} if ((inputsBoxes[1].value<0||inputsBoxes[1].value>100||inputsBoxes[1].value==='')&&thereA0Percent===1) {
+		} if ((inputsBoxes[1].value < 0 || inputsBoxes[1].value > 100 || inputsBoxes[1].value === '') && thereA0Percent === 1) {
 			internalBox[i].parentNode.classList.add('error');
 			openQuestion(internalBox[i].parentNode.querySelector('.external'));
 			inputsBoxes[1].classList.add('become-red');
@@ -512,23 +514,24 @@ function validityLevels(){
 			internalBox[i].querySelector('textarea').insertAdjacentHTML("afterend", "<h1>A descrição do nível deve ter no mínimo 30 caracteres.</h1>");
 		}
 	}
-	for (ii=0; ii<internalBox.length;ii++){
-		if (!internalBox[ii].parentNode.classList.contains('error')){
-			closeQuestion(internalBox[ii].parentNode.querySelector('.external'));}
+	for (ii = 0; ii < internalBox.length; ii++) {
+		if (!internalBox[ii].parentNode.classList.contains('error')) {
+			closeQuestion(internalBox[ii].parentNode.querySelector('.external'));
+		}
 	}
 	if (valityValue === 1)
 		goToCreateLevel();
 	else
-		window.scrollTo(0,0);
+		window.scrollTo(0, 0);
 }
 
 function toggleQuestion(element) {
 	const internal = element.parentNode.querySelector('.internal');
 	const ion = element.querySelectorAll('ion-icon');
 	internal.classList.toggle('hidden');
-	for(let i=0;i<ion.length;i++){
-        ion[i].classList.toggle('hidden');
-    }
+	for (let i = 0; i < ion.length; i++) {
+		ion[i].classList.toggle('hidden');
+	}
 }
 function openQuestion(element) {
 	const internal = element.parentNode.querySelector('.internal');
@@ -549,21 +552,21 @@ function closeQuestion(element) {
 function validarInfosQuizz() {
 	let valityValue = 1;
 	arrayCreateQuizz = {
-		titulo:'',
-		imagem:'',
+		titulo: '',
+		imagem: '',
 		quantidadeDePerguntas: 0,
 		quantidadeDeNiveis: 0,
 		perguntas: [],
-		niveis:[],
+		niveis: [],
 	}
 	let allInputs = create.querySelectorAll('input');
 	let allH1 = create.querySelectorAll('h1');
-	for (let i=0; i<allInputs.length;i++){
+	for (let i = 0; i < allInputs.length; i++) {
 		allInputs[i].classList.remove('become-red');
 		allInputs[i].classList.remove('become-red-2');
 		allInputs[i].classList.remove('become-red-3');
 	}
-	for (i=0; i<allH1.length;i++){
+	for (i = 0; i < allH1.length; i++) {
 		allH1[i].remove();
 	}
 	const titulo = document.querySelector("#text");
@@ -585,7 +588,7 @@ function validarInfosQuizz() {
 		inputsBoxes[1].classList.add('become-red');
 		valityValue = 0;
 		inputsBoxes[1].insertAdjacentHTML("afterend", "<h1>O valor informado não é uma URL válida.</h1>");
-    } if (arrayCreateQuizz.quantidadeDePerguntas < 3) {
+	} if (arrayCreateQuizz.quantidadeDePerguntas < 3) {
 		inputsBoxes[2].classList.add('become-red');
 		valityValue = 0;
 		inputsBoxes[2].insertAdjacentHTML("afterend", "<h1>Quantidade mínima de perguntas 3.</h1>");
@@ -595,5 +598,23 @@ function validarInfosQuizz() {
 		inputsBoxes[3].insertAdjacentHTML("afterend", "<h1>Quantidade mínima de níveis 2.</h1>");
 	}
 	if (valityValue === 1)
-    	goToCreateQuestion();
+		goToCreateQuestion();
+}
+
+function deletar() {
+	let confirmar = confirm('Confirme para excluir o quizz');
+	//SELECIONAR A IMAGEM E ENVIAR O ID DO QUIZZ NA REQUISIÇÃO 
+	const imagem = document.querySelector('.quizz-box');
+	if (confirmar === true) {
+		//então envio a requisição
+		confirmar = axios.delete(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${ID_DO_QUIZZ}, {headers: {"Secret-Key": ${value}}`);
+		confirmar.then(mostrarResposta);
+    }
+}
+deletar();
+function mostrarResposta(response) {
+	if (response.status === 200) {
+		alert('Quizz excluído com sucesso');
+		//chamar lista de quizzes do usuário novamente;
+	}
 }
